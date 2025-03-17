80d1ab03c7d45f33390635fe73de1b538a08f5d9
poojanOctet <poojan@octet.design>
2025-03-17 15:23:26 +0530

    requirements.txt modified

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install missing system dependencies
-# apt-get update && apt-get install -y \
-#     libgtk-4-dev \
-#     libgraphene-1.0-0 \
-#     libgstreamer-gl1.0-0 \
-#     libgstreamer-plugins-bad1.0-0 \
-#     libenchant-2-2 \
-#     libsecret-1-0 \
-#     libmanette-0.2-0 \
-#     libgles2-mesa
+apt-get update && apt-get install -y \
+    libgtk-4-dev \
+    libgraphene-1.0-0 \
+    libgstreamer-gl1.0-0 \
+    libgstreamer-plugins-bad1.0-0 \
+    libenchant-2-2 \
+    libsecret-1-0 \
+    libmanette-0.2-0 \
+    libgles2-mesa
 
 # Install playwright browsers
 python -m playwright install --with-deps
